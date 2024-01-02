9b135e15fa70e32bf540c66450fb1c823e0d04d3
Yannick Meillier <meillier_yannick@yahoo.fr>
2024-01-02 18:50:49 +0000

    label namespace with istio.rev and update set ISTIO_REV automatically

--- scripts/task-16.sh
+++ scripts/task-16.sh
@@ -7,7 +7,7 @@ GKE_DEV_CLUSTER_NAME=cymbal-bank-dev
 OUTPUT_DIR=../asm
 ASMCLI_INSTALL_URL=https://storage.googleapis.com/csm-artifacts/asm/asmcli_1.16 
 ASMCLI_CMD=./asmcli
-ISTIO_REV=asm-1172-1
+#ISTIO_REV=asm-1172-1
 NAMESPACE=default
 
 # download asmcli fresh to use it
@@ -27,14 +27,17 @@ $ASMCLI_CMD install \
 gcloud container clusters get-credentials $GKE_DEV_CLUSTER_NAME \
   --project=$PROJECT_ID --zone=$ZONE
 
+
 # NOTE!!!!!!!!!!!!!!!!!!!!
 # Make sure the ISTIO_REV is correct by running this on the prod cluster after ASM install
 # Look for the 'istio.io/rev' label
 # kubectl get pods -n istio-system --show-labels
+ISTIO_REV=$(kubectl get deploy -n istio-system -l app=istiod -o jsonpath={.items[*].metadata.labels.'istio\.io\/rev'}'{"\n"}')
 
 # Add sidecar injection to all our namespaces
 # NOTE: don't worry if it couldn't find istio-injection, we're just trying to remove it IF it's there
 kubectl label namespace $NAMESPACE istio-injection- istio.io/rev=$ISTIO_REV --overwrite
+kubectl label namespace $NAMESPACE istio.io/rev=$ISTIO_REV --overwrite
 
 # restart all pods
 kubectl rollout restart deployment
