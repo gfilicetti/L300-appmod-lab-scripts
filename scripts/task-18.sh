7f2da51ce4e765239680aa28b3fb601f8483ba19
Yannick Meillier <meillier_yannick@yahoo.fr>
2024-01-02 18:53:43 +0000

    auto set context prod and dev and remove typo in istioctl (x)

--- scripts/task-18.sh
+++ scripts/task-18.sh
@@ -6,8 +6,8 @@ ZONE=us-central1-a
 GKE_PROD_CLUSTER_NAME=cymbal-bank-prod
 GKE_DEV_CLUSTER_NAME=cymbal-bank-dev
 CONFIG_MAP_NAME=asm-options
-CONTEXT_PROD=gke_qwiklabs-gcp-02-7f381c0aae13_us-central1-a_cymbal-bank-prod
-CONTEXT_DEV=gke_qwiklabs-gcp-02-7f381c0aae13_us-central1-a_cymbal-bank-dev
+CONTEXT_PROD=gke_${PROJECT_ID}_${ZONE}_cymbal-bank-prod
+CONTEXT_DEV=gke_${PROJECT_ID}_${ZONE}_cymbal-bank-dev
 
 # NOTE !!!!!!!!!!!!!!!!!!!!!!
 # Use this command to list out all the contexts in your kube config
@@ -27,13 +27,13 @@ kubectl --context=$CONTEXT_DEV create configmap $CONFIG_MAP_NAME -n istio-system
 PRIV_IP_PROD=`gcloud container clusters describe "${GKE_PROD_CLUSTER_NAME}" \
   --zone "${ZONE}" --format "value(privateClusterConfig.privateEndpoint)"`
 
-../asm/istioctl x create-remote-secret --context=${CONTEXT_PROD} --name=${GKE_PROD_CLUSTER_NAME} --server=https://${PRIV_IP_PROD} > ${CONTEXT_PROD}.secret
+../asm/istioctl create-remote-secret --context=${CONTEXT_PROD} --name=${GKE_PROD_CLUSTER_NAME} --server=https://${PRIV_IP_PROD} > ${CONTEXT_PROD}.secret
 
 # create remote secret for dev
 PRIV_IP_DEV=`gcloud container clusters describe "${GKE_DEV_CLUSTER_NAME}" \
   --zone "${ZONE}" --format "value(privateClusterConfig.privateEndpoint)"`
 
-../asm/istioctl x create-remote-secret --context=${CONTEXT_DEV} --name=${GKE_DEV_CLUSTER_NAME} --server=https://${PRIV_IP_DEV} > ${CONTEXT_DEV}.secret
+../asm/istioctl create-remote-secret --context=${CONTEXT_DEV} --name=${GKE_DEV_CLUSTER_NAME} --server=https://${PRIV_IP_DEV} > ${CONTEXT_DEV}.secret
 
 # apply the remote secrets
 kubectl apply -f ${CONTEXT_PROD}.secret --context=${CONTEXT_DEV}
