464eb210a97161be70f654b30df86c5889ebec8b
Yannick Meillier <meillier_yannick@yahoo.fr>
2024-01-02 18:46:04 +0000

    automatically set ISTIO_REV and removed space typo in injection command

--- scripts/task-15.sh
+++ scripts/task-15.sh
@@ -4,7 +4,7 @@
 PROJECT_ID=$(gcloud config get-value project)
 ZONE=us-central1-a
 GKE_PROD_CLUSTER_NAME=cymbal-bank-prod
-ISTIO_REV=asm-1172-1
+#ISTIO_REV=asm-1172-1
 NAMESPACE=default
 
 # get kubectl context for this cluster
@@ -15,10 +15,13 @@ gcloud container clusters get-credentials $GKE_PROD_CLUSTER_NAME \
 # Make sure the ISTIO_REV is correct by running this on the prod cluster after ASM install
 # Look for the 'istio.io/rev' label
 # kubectl get pods -n istio-system --show-labels
+ISTIO_REV=$(kubectl get deploy -n istio-system -l app=istiod -o \
+jsonpath={.items[*].metadata.labels.'istio\.io\/rev'}'{"\n"}')
+
 
 # Add sidecar injection to all our namespaces
 # NOTE: don't worry if it couldn't find istio-injection, we're just trying to remove it IF it's there
-kubectl label namespace $NAMESPACE istio-injection- istio.io/rev=$ISTIO_REV --overwrite
+kubectl label namespace $NAMESPACE istio-injection-istio.io/rev=$ISTIO_REV --overwrite
 
 # restart all pods
 kubectl rollout restart deployment
